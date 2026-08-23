14c8f4ef08eb68b9163041786cf1278eb0f5817b
Mahmoud <mahmoudnasser1561@gmail.com>
2026-08-23 22:35:50 +0300

    feat(page-cache): implement page caching functions using redis

---
+++ src/services/keys.ts
@@ -0,0 +1,2 @@
+export const pageCacheKey = (id: string) => `pagecache#${id}`;
+
--- src/services/queries/page-cache.ts
+++ src/services/queries/page-cache.ts
@@ -1,3 +1,23 @@
-export const getCachedPage = (route: string) => {};
+import { pageCacheKey } from "$services/keys";
+import { client } from "$services/redis";
 
-export const setCachedPage = (route: string, page: string) => {};
+const cachedRoutes = [
+    '/about', '/privacy', '/auth/signin', '/auth/signup'
+];
+
+export const getCachedPage = (route: string) => {
+    if (cachedRoutes.includes(route)) {
+        client.get(pageCacheKey(route));
+    }
+
+    return null;
+};
+
+export const setCachedPage = (route: string, page: string) => {
+    if (cachedRoutes.includes(route)) {
+        return client.set(pageCacheKey(route), page, {
+            // TODO: increase exp value
+            EX: 2
+        })
+    }
+};
