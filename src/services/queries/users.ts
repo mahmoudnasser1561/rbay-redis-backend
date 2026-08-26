abcd457683e58772e275cb23d31c2f32d0372a45
Mahmoud <mahmoudnasser1561@gmail.com>
2026-08-26 09:51:21 +0300

    feat(users): add reusable deserialize function for user attributes

--- src/services/queries/users.ts
+++ src/services/queries/users.ts
@@ -6,7 +6,11 @@ import { attr } from 'svelte/internal';
 
 export const getUserByUsername = async (username: string) => {};
 
-export const getUserById = async (id: string) => {};
+export const getUserById = async (id: string) => {
+    const user = await client.hGetAll(userCacheKey(id));
+
+    return deserialize(id, user);
+};
 
 export const createUser = async (attrs: CreateUserAttrs) => {
     const id = genId();
@@ -20,4 +24,12 @@ const serialize = (user: CreateUserAttrs) => {
         username: user.username,
         password: user.password
     }
+};
+
+const deserialize = (id: string, user: { [key: string]: string })  => {
+    return {
+        id: id,
+        user: user.username,
+        password: user.password
+    };
 };
