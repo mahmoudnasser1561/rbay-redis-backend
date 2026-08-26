cf7290c14de500801f0f0e3ced51945e1c424dcb
Mahmoud <mahmoudnasser1561@gmail.com>
2026-08-26 10:59:25 +0300

    feat(items): implement createItem function and item serialization

--- src/services/keys.ts
+++ src/services/keys.ts
@@ -1,5 +1,4 @@
 export const pageCacheKey = (id: string) => `pagecache#${id}`;
-
 export const userCacheKey = (userId: string) => `users#${userId}`;
-
 export const sessionCacheKey = (sessionId: string) => `sessions#${sessionCacheKey}`;
+export const itemsKey = (itemId: string) => `items#${itemId}`;
