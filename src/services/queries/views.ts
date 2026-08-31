72afef640c3d5d8d261335465cdbc0177fabf29e
Mahmoud <mahmoudnasser1561@gmail.com>
2026-08-31 11:19:16 +0300

    implemented unique user views using hyperloglog

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -1,9 +1,14 @@
 import { client } from '$services/redis';
-import { itemsKey, itemsByViewsKey } from '$services/keys';
+import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
+import { insert } from 'svelte/internal';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	return Promise.all([
-		client.hIncrBy(itemsKey(itemId), 'views', 1),
-		client.zIncrBy(itemsByViewsKey(), 1, itemId)
-	]);
+	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
+	
+	if (inserted) {
+		return Promise.all([
+			client.hIncrBy(itemsKey(itemId), 'views', 1),
+			client.zIncrBy(itemsByViewsKey(), 1, itemId)
+		]);
+	}
 };
