0d689a9e23693437eba0a10da9277b6b0b404c8d
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-02 16:56:07 +0300

    replaced the round-trip incrementView with lua script function

--- src/services/queries/views.ts
+++ src/services/queries/views.ts
@@ -3,12 +3,5 @@ import { itemsKey, itemsByViewsKey, itemsViewsKey } from '$services/keys';
 import { insert } from 'svelte/internal';
 
 export const incrementView = async (itemId: string, userId: string) => {
-	const inserted = await client.pfAdd(itemsViewsKey(itemId), userId);
-	
-	if (inserted) {
-		return Promise.all([
-			client.hIncrBy(itemsKey(itemId), 'views', 1),
-			client.zIncrBy(itemsByViewsKey(), 1, itemId)
-		]);
-	}
+	return client.incrementView(itemId, userId);
 };
