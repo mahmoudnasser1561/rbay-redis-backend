99c3c49dd48b49b8444090b8c1f68b917374bf3a
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-02 17:36:55 +0300

    use withLock custom lock implementation to guard against concurrency issues which provides a higher success rate than the watched pipeline

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -1,47 +1,46 @@
 import type { CreateBidAttrs, Bid } from '$services/types';
 import { bidHistoryKey, itemsKey, itemsByPriceKey } from '$services/keys';
-import { client } from '$services/redis';
+import { client, withLock } from '$services/redis';
 import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return client.executeIsolated(async (isolatedClient) => {
-		await isolatedClient.watch(itemsKey(attrs.itemId));
-		
+	return withLock(attrs.itemId, async () => {
+		// 1) fetch data
+		// 2) do validation 
+		// 3) writing data
 		const item = await getItem(attrs.itemId);
-		
 		if (!item) {
 			throw new Error('Item does not exist');
 		}
-
+		
 		if (item.price >= attrs.amount) {
 			throw new Error('Bid too low');
 		}
-
+		
 		if (item.endingAt.diff(DateTime.now()).toMillis() < 0) {
 			throw new Error('Item closed to bidding');
 		}
-		
+	
 		const serialized = serializeHistory(
 			attrs.amount,
 			attrs.createdAt.toMillis() 
 		)
 
-		return isolatedClient
-			.multi()
-			.rPush(bidHistoryKey(attrs.itemId), serialized)
-			.hSet(itemsKey(item.id), {
-					bids: item.bids + 1,
-					price: attrs.amount,
-					highestBidUserId: attrs.userId
-				})
-			.zAdd(itemsByPriceKey(), {
+		Promise.all([
+			client.rPush(bidHistoryKey(attrs.itemId), serialized),
+			client.hSet(itemsKey(item.id), {
+				bids: item.bids + 1,
+				price: attrs.amount,
+				highestBidUserId: attrs.userId
+			}),
+			client.zAdd(itemsByPriceKey(), {
 				value: item.id,
 				score: attrs.amount
 			})
-			.exec()
-	});
-};
+		]);
+	})
+}
 
 export const getBidHistory = async (itemId: string, offset = 0, count = 10): Promise<Bid[]> => {
 	const startIndex = -1 * offset  - count;
