52636f99a1487a6a842d5dea39783b5c256cdf98
Mahmoud <mahmoudnasser1561@gmail.com>
2026-09-02 19:06:37 +0300

    create a proxied client to guard against concurrency issues rather than explicit signal check

--- src/services/queries/bids.ts
+++ src/services/queries/bids.ts
@@ -5,7 +5,7 @@ import { DateTime } from 'luxon';
 import { getItem } from './items';
 
 export const createBid = async (attrs: CreateBidAttrs) => {
-	return withLock(attrs.itemId, async () => {
+	return withLock(attrs.itemId, async (lockedClient: typeof client ,signal: any) => {
 		// 1) fetch data
 		// 2) do validation 
 		// 3) writing data
@@ -27,14 +27,18 @@ export const createBid = async (attrs: CreateBidAttrs) => {
 			attrs.createdAt.toMillis() 
 		)
 
+		// if (signal.expired) {
+		// 	throw new Error('Lock Expired, cant write more data' )
+		// }
+
 		Promise.all([
-			client.rPush(bidHistoryKey(attrs.itemId), serialized),
-			client.hSet(itemsKey(item.id), {
+			lockedClient.rPush(bidHistoryKey(attrs.itemId), serialized),
+			lockedClient.hSet(itemsKey(item.id), {
 				bids: item.bids + 1,
 				price: attrs.amount,
 				highestBidUserId: attrs.userId
 			}),
-			client.zAdd(itemsByPriceKey(), {
+			lockedClient.zAdd(itemsByPriceKey(), {
 				value: item.id,
 				score: attrs.amount
 			})
